d94c5189e85eaaea491b95d1c48efe8fd9f415ea
hizulu <lucia.gl.vega@gmail.com>
2026-04-09 22:25:15 +0200

    Creación del proyecto MAUI

---
+++ Practica2LuciaGarciaLopez/Practica2LuciaGarciaLopez/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Practica2LuciaGarciaLopez
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Practica2LuciaGarciaLopez/Practica2LuciaGarciaLopez/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica2LuciaGarciaLopez
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
